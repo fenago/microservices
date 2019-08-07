8796e27ab16f5ae83c741de006fb6d4bce166dc7
Asad Riaz <dev.asadriaz@gmail.com>
2019-08-07 13:44:23 -0700

    part

---
+++ part 1 scaling microservice/rng/src/main/java/com/learningvoyage/rngservice/HelloController.java
@@ -0,0 +1,35 @@
+
+package com.learningvoyage.rngservice;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RestController
+public class HelloController {
+    
+   
+        @RequestMapping("/rng")
+	@ResponseBody
+	String home() {
+		return "RNG running on rng";
+	}
+	@RequestMapping("/rng/{howMany}")
+	@ResponseBody
+	public String getFRandomNumber(
+			  @PathVariable("howMany") int howMany) {
+		
+		SecureRandom rnd = new SecureRandom();
+		byte[] token = new byte[howMany];
+		rnd.nextBytes(token);
+			    return  Base64.getEncoder().encodeToString(token);
+			}
+	    
+}
+
